21d1ab41d251e75ca802ae840c521896b73ed3cf
Roman S <roman.sukhopleshchenko@yandex.ru>
2022-08-15 18:11:37 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,21 @@
 class GuessingGame {
     constructor() {}
-
+        
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return Math.round((this.max + this.min)/2);
     }
 
     lower() {
-
+        this.max = this.guess();
     }
 
     greater() {
-
+        this.min = this.guess();
     }
 }
 
